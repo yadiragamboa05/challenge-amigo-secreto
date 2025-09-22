8f8ab683ba78bfa82b9b3ea0241281bf16b50271
yadiragamboa05 <yadiragamboaq.05@gmail.com>
2025-09-21 23:38:37 -0500

    Actualizado todos los puntos requeridos

--- app.js
+++ app.js
@@ -2,12 +2,40 @@
 let amigos = [];
 
 function agregarAmigo() {
-    let nuevoAmigo = document.getElementById('amigo').value;
+    const nuevoAmigo = document.getElementById('amigo').value;
 
     if (nuevoAmigo === "") {
         alert("Por favor, inserte un nombre.");
     } else {
         amigos.push(nuevoAmigo);
         document.getElementById('amigo').value = '';
+        mostrarAmigos();
+    }
+}
+
+function mostrarAmigos() {
+    const lista = document.getElementById('listaAmigos');
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    const resultado = document.getElementById('resultado');
+
+    if (amigos.length === 0) {
+        resultado.innerHTML = "No hay amigos disponibles para el sorteo.";
+        return;
+    } else {
+        const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        const amigoSorteado = amigos[indiceAleatorio];
+
+        resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
     }
 }
