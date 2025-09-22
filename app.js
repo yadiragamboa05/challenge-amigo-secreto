f14f20b7e3211055999454d393aa0beb504c6547
yadiragamboa05 <yadiragamboaq.05@gmail.com>
2025-09-21 23:13:34 -0500

    Actualizando indicación 1

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById('amigo').value;
+
+    if (nuevoAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nuevoAmigo);
+        document.getElementById('amigo').value = '';
+    }
+}
